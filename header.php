02550603c7d60e07871b97465e13401245065e79
SamuelSchwegler <samuel.schwegler@gmx.ch>
2021-12-19 09:21:19 +0100

    mobile menu, noch nicht fertig

--- header.php
+++ header.php
@@ -14,31 +14,50 @@
             src="https://code.jquery.com/jquery-3.6.0.min.js"
             integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
             crossorigin="anonymous"></script>
-    <script type="text/javascript" src="<?php if (function_exists('get_template_directory_uri')) {
-        echo get_template_directory_uri() . '/';
-    } ?>resources/js/menu-bar.js" defer></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/public/js/script.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/public/js/defer.js" defer></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;500&display=swap" rel="stylesheet">
 </head>
 <body>
 <div id="page">
+    <button class="menu-toggle"></button>
     <div id="navigation-bar">
         <div id="logo-area">
             <?php if (function_exists('the_custom_logo')) {
                 the_custom_logo();
             } ?>
         </div>
-        <?php
-        wp_nav_menu([
-            'menu' => 'main',
-            'container_id' => 'menu-area',
-            'walker' => new CSS_Menu_Maker_Walker()
-        ]);
-        ?>
-        <div class="fill-area">
+        <div class="mobile-menu">
+            <?php
+            wp_nav_menu([
+                'menu' => 'main',
+                'container_id' => 'main-menu-mobile',
+                'walker' => new CSS_Menu_Maker_Walker()
+            ]);
+            ?>
+            <div id="mobile-search-form" class="search-form-outer">
+                <div class="search-form-wrapper">
+                    <form method="get" id="searchform-mobile" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                        <input type="text" class="form-control" style="color:white;" value="<?php echo wp_specialchars($s, 1); ?>" placeholder="Suchen..." name="s" id="s-mobile" />
+                    </form>
+                    <div class="search-form-icon"><a id="search-icon-wrapper"><i class="fas fa-search"></i></a></div>
+                </div>
+            </div>
         </div>
-        <div class="events">
+        <div class="hide-mobile">
+            <?php
+            wp_nav_menu([
+                'menu' => 'main',
+                'container_id' => 'menu-area',
+                'walker' => new CSS_Menu_Maker_Walker()
+            ]);
+            ?>
+        </div>
+        <div class="fill-area hide-mobile">
+        </div>
+        <div class="events hide-mobile">
             <h5>Nächste Termine</h5>
             <?php
             if (function_exists('calendar_events')) {
@@ -53,6 +72,6 @@
             ?>
         </div>
     </div>
-    <div class="shadow-element"></div>
-    <div id="overlay"></div>
+    <div class="shadow-element hide-mobile"></div>
+    <div id="overlay hide-mobile"></div>
     <div id="main">
