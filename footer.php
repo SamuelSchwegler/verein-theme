8ecb760938bae7c01561fad48f8257305962d895
SamuelSchwegler <samuel.schwegler@gmx.ch>
2022-05-24 21:49:10 +0200

    config footer

--- footer.php
+++ footer.php
@@ -1,16 +1,22 @@
+<?php
+    $json = file_get_contents(__DIR__.'/config.json');
+    $config = json_decode($json, 1);
+?>
             <div id="footer">
                 <div class="row">
                     <div class="col">
                         <h3>Links</h3>
                         <p>
-                            <a href="#">turnverband.ch</a>
+                            <?php foreach($config->footer_links as $from => $link) { ?>
+                                <a href="<?php echo $link; ?>"><?php echo $link; ?></a><br>
+                            <?php } ?>
                         </p>
                     </div>
                     <div class="col">
                         <h3>Kontakt</h3>
-                        Samuel Schwegler</br>
-                        Willisauerstrasse 11</br>
-                        6122 Menznau
+                        <?php echo $config->footer_address->name; ?></br>
+                        <?php echo $config->footer_address->street; ?></br>
+                        <?php echo ($config->footer_address->postcode ?? '')." ".$config->footer_address->city; ?>
                     </div>
                     <div class="col"></div>
                 </div>
