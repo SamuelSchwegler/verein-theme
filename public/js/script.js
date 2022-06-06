859c43671021996231f25f1b784b9c9cf8d9f131
SamuelSchwegler <samuel.schwegler@gmx.ch>
2022-06-06 11:40:22 +0200

    removed log texts in script

--- public/js/script.js
+++ public/js/script.js
@@ -8,12 +8,10 @@ jQuery(function () {
 
     // left bar
     jQuery('.hide-mobile .menu-item-has-children a').click(function (event) {
-        console.log('open');
         toggleMenu(jQuery(this));
     });
 
     jQuery('body:not(.nav-is-toggled) #navigation-bar').click(function (event) {
-        console.log('close');
         toggleMenu(jQuery(this), 'close');
     });
 
@@ -22,7 +20,6 @@ jQuery(function () {
     });
 
     jQuery('#menu-area.menu-main-container').click(function () {
-        console.log('bla bla');
         var elem = jQuery('.current_page_item').first();
         //funktioniert
         if (elem.parent().hasClass('nav-expand-content')) {
